11f169921e5780f565bd518bce9c725c2836889e
KennethFam <147673498+KennethFam@users.noreply.github.com>
2024-04-28 16:10:12 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,2 @@
 --start here
+SELECT product_description as Product_Recalled, product_type as Product_Type, reason_for_recall as Reason, recall_initiation_date as Date_Started FROM `bigquery-public-data.fda_food.food_enforcement` ORDER BY recall_initiation_date DESC LIMIT 10
